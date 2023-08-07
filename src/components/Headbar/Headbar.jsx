ea391be4c9561c5ca0d2e9c42d0abb6366ac299a
Andrii Protasenko <andrii.protas58@gmail.com>
2023-08-07 22:04:46 +0300

    feature: Adding logo

--- src/components/Headbar/Headbar.jsx
+++ src/components/Headbar/Headbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import sc from "./Headbar.module.css";
-
+import logo from "../../assets/logofull.svg";
 
 const Headbar = () => {
-  return <div className={sc.main}></div>;
+  return (
+    <div className={sc.main}>
+      <div className={sc.conLogo}>
+        <img src={logo} alt="Digital project" />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Headbar;
