5e430d4ec3d9a8fb209adf33313f7617cfe05b69
Andrii Protasenko <andrii.protas58@gmail.com>
2023-08-10 17:20:12 +0300

    feature: add links in head bar

--- src/components/Headbar/Headbar.jsx
+++ src/components/Headbar/Headbar.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import sc from "./Headbar.module.css";
 import logo from "../../assets/logofull.svg";
 
 const Headbar = () => {
+  const [headbarbuttons, setHeadbarbutton] = useState([
+    { name: "Главная", url: "https://www.google.com" },
+    { name: "Галерея", url: "https://www.google.com" },
+    { name: "Проекты", url: "https://www.google.com" },
+    { name: "Сертификаты", url: "https://www.google.com" },
+    { name: "Контакты", url: "https://www.google.com" },
+  ]);
+
   return (
     <div className={sc.main}>
-      <div className={sc.conLogo}>
-        <img src={logo} alt="Digital project" />
+      <img className={sc.conLogo} src={logo} alt="Digital project" />
+      <div>
+        {headbarbuttons.map((e) => {
+          return (
+            <a className={sc.mainlinks} url={e.url}>
+              {e.name.toUpperCase()}
+            </a>
+          );
+        })}
       </div>
-      <div></div>
     </div>
   );
 };
