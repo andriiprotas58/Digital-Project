19b744477b52a55bb0280e4f6696ad9b1bdb0e69
Andrii Protasenko <andrii.protas58@gmail.com>
2023-08-11 20:32:00 +0300

    feature: add new component goals

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import sc from "./Content.module.css";
 import Baner from "./Baner/Baner";
 import About from "./About/About";
+import Goals from "./Goals/Goals";
 
 const Content = () => {
   return (
     <div className={sc.main}>
       <Baner className={sc.contentblock} />
       <About className={sc.contentblock} />
+      <Goals className={sc.contentblock}/>
     </div>
   );
 };
