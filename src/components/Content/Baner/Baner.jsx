0ed51a7e7ad47dcf0566dd82f50c4ece18932f66
Andrii Protasenko <andrii.protas58@gmail.com>
2023-08-21 19:33:55 +0300

    feature: add adaptive not full

--- src/components/Content/Baner/Baner.jsx
+++ src/components/Content/Baner/Baner.jsx
@@ -13,53 +13,58 @@ const Baner = ({ className }) => {
 
   const [activeBaner, setActiveBaner] = useState(0);
   return (
-    <div className={className}>
+    <div className={[className, sc.main].join(" ")}>
       <div className={sc.banerText}>
-        <h1>{baners[activeBaner].text.split(" ")[0].toUpperCase()}</h1>
-        <h2>{baners[activeBaner].text.split(" ")[1].toUpperCase()}</h2>
-
-        <div className={sc.buttonBlock}>
-          <button
-            onClick={() => {
-              if (activeBaner - 1 < 0) {
-                setActiveBaner(baners.length - 1);
-              } else {
-                setActiveBaner(activeBaner - 1);
-              }
-            }}
-          >
-            <img src={arrow} alt="" />
-          </button>
-          <button
-            onClick={() => {
-              if (activeBaner + 1 >= baners.length) {
-                setActiveBaner(0);
-              } else {
-                setActiveBaner(activeBaner + 1);
-              }
-            }}
-          >
-            <img
-              style={{
-                MozTransform: "scaleX(-1)",
-                OTransform: "scaleX(-1)",
-                WebkitTransform: "scaleX(-1)",
-                transform: "scaleX(-1)",
-                filter: "flipH",
-              }}
-              src={arrow}
-            />
-          </button>
+        <div className={sc.heading}>
+          <h1>{baners[activeBaner].text.split(" ")[0].toUpperCase()}</h1>
+          <h2>{baners[activeBaner].text.split(" ")[1].toUpperCase()}</h2>
         </div>
 
-        <div className={sc.counter}>
-          <p>
-            {activeBaner + 1 < 10 ? "0" + (activeBaner + 1) : activeBaner + 1}{" "}
-          </p>
-          <div className={sc.line}></div>
-          <p>{baners.length < 10 ? "0" + baners.length : baners.length}</p>
+        <div className={sc.buttandcount}>
+          <div className={sc.buttonBlock}>
+            <button
+              onClick={() => {
+                if (activeBaner - 1 < 0) {
+                  setActiveBaner(baners.length - 1);
+                } else {
+                  setActiveBaner(activeBaner - 1);
+                }
+              }}
+            >
+              <img src={arrow} alt="" />
+            </button>
+            <button
+              onClick={() => {
+                if (activeBaner + 1 >= baners.length) {
+                  setActiveBaner(0);
+                } else {
+                  setActiveBaner(activeBaner + 1);
+                }
+              }}
+            >
+              <img
+                style={{
+                  MozTransform: "scaleX(-1)",
+                  OTransform: "scaleX(-1)",
+                  WebkitTransform: "scaleX(-1)",
+                  transform: "scaleX(-1)",
+                  filter: "flipH",
+                }}
+                src={arrow}
+              />
+            </button>
+          </div>
+
+          <div className={sc.counter}>
+            <p>
+              {activeBaner + 1 < 10 ? "0" + (activeBaner + 1) : activeBaner + 1}{" "}
+            </p>
+            <div className={sc.line}></div>
+            <p>{baners.length < 10 ? "0" + baners.length : baners.length}</p>
+          </div>
         </div>
       </div>
+
       <div className={sc.baner}>
         <img src={baners[activeBaner].img} alt="" />
 
@@ -68,8 +73,6 @@ const Baner = ({ className }) => {
           text={"Взглянуть"}
           href={baners[activeBaner].herf}
         />
-
-        
       </div>
     </div>
   );
